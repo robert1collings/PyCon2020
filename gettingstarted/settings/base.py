3cd6de4eb97555ab8c371cbf733c3ea52fcc3dc0
robertcollings <robertjcollings@gmail.com>
2021-01-16 15:14:49 +0000

    step 3

--- gettingstarted/settings/base.py
+++ gettingstarted/settings/base.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # Whitenoise goes here
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -115,4 +115,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-# Whitenoise static config goes here
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
